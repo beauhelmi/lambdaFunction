e59ff5e46955b27aa44bbd094829cda8847ea46a
beauhelmi <beauhelmi@gmail.com>
2024-11-02 06:32:32 +0800

    lambda function

--- index.php
+++ index.php
@@ -6,43 +6,46 @@
     <title>Document</title>
 </head>
 <body>
-    <?php 
-        $books = [
-            [
-                'name' => 'Do Androids Dream of Electric Sheep',
-                'author' => 'Philip K. Dick',
-                'releaseYear' => 1968,
-                'purchaseUrl' => 'http://example.com'
-            ],
-            [
-                'name' => 'Project Hail Mary',
-                'author' => 'Andy Weir',
-                'releaseYear' => 2021,
-                'purchaseUrl' => 'http://example.com'
-            ],
-            [
-                'name' => 'The Martian',
-                'author' => 'Andy Weir',
-                'releaseYear' => 2011,
-                'purchaseUrl' => 'http://example.com'
-            ],
-        ];
-        
-        function filterByAuthor($books, $author){
-            $filteredBooks = [];
+        <?php 
+            $books = [
+                [
+                    'name' => 'Do Androids Dream of Electric Sheep',
+                    'author' => 'Philip K. Dick',
+                    'releaseYear' => 1968,
+                    'purchaseUrl' => 'http://example.com'
+                ],
+                [
+                    'name' => 'Project Hail Mary',
+                    'author' => 'Andy Weir',
+                    'releaseYear' => 2021,
+                    'purchaseUrl' => 'http://example.com'
+                ],
+                [
+                    'name' => 'The Martian',
+                    'author' => 'Andy Weir',
+                    'releaseYear' => 2011,
+                    'purchaseUrl' => 'http://example.com'
+                ],
+            ];
+            
+            function filter($items, $key, $value) 
+            {
+                $filteredItems = [];
 
-            foreach($books as $book) {
-                if($book['author'] === $author) {
-                    $filteredBooks[] = $book;
+                foreach($items as $item) {
+                    if($item[$key] === $value) {
+                        $filteredItems[] = $item;
+                    }
                 }
-            }
 
-            return $filteredBooks;
-        }
+                return $filteredItems;
+            } 
+            $filteredBooks = filter($books,  'releaseYear', 2021);
         ?>
 
+
         <ul>
-            <?php foreach (filterByAuthor($books, 'Philip K. Dick') as $book) : ?>
+            <?php foreach ($filteredBooks as $book) : ?>
                 <li>
                     <a href="<?= $book['purchaseUrl']?>">
                         <?= $book['name']; ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
